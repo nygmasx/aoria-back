c4f3e4bee1cb712b7a0dd7c12f91c9d48f7600fd
nygmasx <imraneeslk@gmail.com>
2024-06-19 13:34:41 +0100

    logout route

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,28 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::group(["middleware" => ["auth:sanctum"]], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post("/test-csrf", fn() => [1, 2, 3]);
+
+    Route::post("/logout", function (Request $request) {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->noContent();
+    });
+});
 
 Route::get('/users', function (Request $request) {
-    return \App\Models\User::all();
+    return User::all();
 });
 
 Route::post('/login', function (Request $request) {
@@ -20,7 +32,7 @@ Route::post('/login', function (Request $request) {
             'device_name' => ['required'],
         ]
     );
-    $user = \App\Models\User::where('email', $request->email)->first();
+    $user = User::where('email', $request->email)->first();
 
     if (!$user || !Hash::check($request->password, $user->password)) {
         throw ValidationException::withMessages([
